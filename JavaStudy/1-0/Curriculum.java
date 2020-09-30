1990f5848db23e69815321557671d5ad0d76427a
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2020-09-30 21:38:34 +0900

    課題提出
    データ型、文字列クラス

---
+++ JavaStudy/1-0/Curriculum.java
@@ -0,0 +1,9 @@
+public class Curriculum{
+    public static void main(String[] args){
+
+//int型を宣言して、「seisu」という変数名にして、「1234」で初期化する。
+      int seisu = 1234;
+      System.out.println("int型 = 整数 = " + 1234);
+
+    }
+}
---
+++ JavaStudy/1-0/Curriculum2.java
@@ -0,0 +1,8 @@
+public class Curriculum2{
+    public static void main(String[] args){
+        //Stringクラスを宣言して、mojiは変数で、「javaはプログラム言語」で初期化。
+        String moji = "javaはプログラム言語";
+
+        System.out.print(moji);
+    }
+}
