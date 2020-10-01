1fd5f9d74877104df5f038f8ebaa886a35138ba9
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2020-10-01 21:04:34 +0900

    配列例題
    配列例題

---
+++ JavaStudy/1-2/hairetsu.java
@@ -0,0 +1,6 @@
+public class hairetsu{
+    public static void main(String[] args){
+        int[] arr = {60,70,80};
+        System.out.println("配列arrの配列数..."+arr.length);
+         }
+}
---
+++ JavaStudy/1-2/hairetsu2.java
@@ -0,0 +1,10 @@
+public class hairetsu2{
+    public static void main(String[] args){
+        //int[]型変数scoreの宣言と初期化
+        int score[] = { 80,70,90 };
+        //合計スコアを宣言し、0で初期化
+        int sum = 0;
+        sum = score[0] + score[1] + score[2];
+        System.out.println("合計点は"+sum+ "点です");
+        }
+}
