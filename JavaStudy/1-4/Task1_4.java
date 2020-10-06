f7c3cd12405239540b3f144e364b1c5b8bb30b3c
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2020-10-06 20:30:30 +0900

    課題の提出
    制御文（条件分岐）

--- JavaStudy/1-4/Task1_4.java
+++ JavaStudy/1-4/Task1_4.java
@@ -23,13 +23,13 @@ public class Task1_4{
         String pass = "pass";
         // ② 名前が「 alice 」、パスワードが「 pass 」であった場合、
         //「 ログイン成功です 」と出力してください。
-        if( name == "alice" && pass == "pass"){
+        if( name.equals("alice") && pass.equals("pass")){
             System.out.println(CONST_MSG_SUCCESS);
         // ③ 名前のみ正しい場合、「 パスワードに誤りがあります。 」と出力してください。
-        }else if( name == "alice" && pass != "pass"){
+        }else if( name.equals("alice") && pass != "pass"){
             System.out.println(CONST_MSG_ERROR_PASS);
         // ④ パスワードのみ正しい場合、「 名前に誤りがあります。 」と出力してください。
-        }else if( name != "alice" && pass == "pass"){
+        }else if( name != "alice" && pass.equals("pass")){
             System.out.println(CONST_MSG_ERROR_NAME);
         // ⑤ 名前もパスワードも間違っていた場合は、「 入力情報に誤りがあります。 」と出力してください。
         }else{
