6ebd003a99b5e9ede3641f380eecbfca2a7e68e4
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2020-11-07 17:55:00 +0900

    課題の再提出
    日付の操作

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -38,8 +38,8 @@ public class Study {
         // - この課題に関しては課題実施日によって結果が異なりますので、Wiki課題の画像とは結果が異なります。
         // - また、うるう年の場合は設定内容が正しくとも日付がズレて表示される可能性があります。
         System.out.println(calendar.get(Calendar.YEAR)+1);
-        calendar.add(Calendar.MONTH, 3);
-        System.out.println(calendar.get(Calendar.MONTH));
+        calendar.add(Calendar.MONTH, 2);
+        System.out.println(calendar.get(Calendar.MONTH)+1);
         System.out.println(calendar.get(Calendar.DATE)+15);
 
 
