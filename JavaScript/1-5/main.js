4f2ba5f5d9e2b32c15a52939966b046b76419fc7
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2021-02-07 18:10:48 +0900

    課題提出
    js1-5

---
+++ JavaScript/1-5/main.js
@@ -0,0 +1,21 @@
+//関数を定義
+function japanese(){
+    console.log('おはよう');
+    console.log('こんにちは');
+    console.log('こんばんは');
+    console.log('日本語は時間帯によって挨拶が変わります');
+    console.log('ひらがな');
+    console.log('カタカナ');
+    console.log('漢字');
+    console.log('３種類も文字があります');
+}
+japanese();
+
+function createJuice(fruits){
+    fruits = 'みかん'
+    console.log(`${fruits}を受け取りました。ジュースにして返します。`);
+    return orangeJuice;
+}
+let orangeJuice = "みかんジュース";
+createJuice();
+console.log(`${orangeJuice}が届きました`);
