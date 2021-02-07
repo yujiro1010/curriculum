d1e86060b30d881bb1c84e9354d57dc02136a215
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2021-02-07 16:21:25 +0900

    課題提出
    js1-3

---
+++ JavaScript/1-3/main.js
@@ -0,0 +1,25 @@
+//問1
+let a = 8;
+let b = 12;
+if(a !== 10){
+    console.log(a)
+}
+if(b >= 10 && b < 20 && b % 2 === 0){
+    console.log(b);
+}
+
+//問2
+let x = 10;
+if(x >= 10 && x <= 20){
+    console.log("成功です");
+}else{
+    console.log("失敗です");
+}
+
+//問3
+let y = 0;
+if(y % 2 === 0){
+    console.log("偶数です");
+}else if(y % 2 !==0){
+    console.log("奇数です");
+}
