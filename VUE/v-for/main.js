a19816425dc6b21ca9bf1a18ac91ddfe6667bdc4
yujiro1010 <71545515+yujiro1010@users.noreply.github.com>
2021-06-15 17:14:57 +0900

    a

---
+++ VUE/v-for/main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el:"#app",
+    data:{
+        users:[
+            {id:1, name:'taro', age:21},
+            {id:2, name:'jiro', age:22},
+            {id:3, name:'saburo', age:23}
+        ]
+
+    }
+})
